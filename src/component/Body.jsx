6c0ab51493ed4f56bad06653515b8759596813d9
AfzalAbbasi03 <alltry194@gmail.com>
2024-09-04 11:48:21 +0530

    something change that i dont know

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,11 +1,7 @@
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/userSlice";
+
 
 const Body = () => {
   const appRouter = createBrowserRouter([
